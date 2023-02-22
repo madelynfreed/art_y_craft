cdc2cbaef6847d343b42fce6377336859997344c
Madelyn Freed <madelyn.freed@gusto.com>
2023-02-21 21:02:13 -0500

    init pts repo

---
+++ parabolas/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const AwesomeTypescriptLoader = require('awesome-typescript-loader');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader', exclude: path.join(process.cwd(), 'node_modules/pts') },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'PTS',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+};
